6d4791d95fdd5293f8dfb7dd5ba1d4aa8b87de5c
ChenJing <502269499@qq.com>
2018-09-25 10:42:33 +0800

    rem由百分比的方式改为使用px

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,7 +2,7 @@ let windowResizeWidth
 window.onresize = function (e) {
   // 当宽度变化时才有必要更改fontSize
   if (document.documentElement.clientWidth !== windowResizeWidth) {
-    document.querySelector('html').style.fontSize = document.documentElement.clientWidth / 375 * 312.5 + '%'
+    document.querySelector('html').style.fontSize = document.documentElement.clientWidth / 375 * 16 + 'px'
     windowResizeWidth = document.documentElement.clientWidth
   }
 }
